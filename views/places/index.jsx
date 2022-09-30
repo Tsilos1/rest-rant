1bb257898d95a25a0681609efe1e607f676d560b
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-30 15:05:42 -0500

    got it working again. Places page

---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>Index Page</h1>
+              <p>This is the index page</p>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = index
