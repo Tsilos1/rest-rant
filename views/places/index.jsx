825b422fe483be0d6f8f5778a3d087603e88acc5
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-30 19:59:55 -0500

    added places with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,16 +1,25 @@
 const React = require('react')
 const Def = require('../default')
 
-function index () {
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
     return (
       <Def>
           <main>
-              <h1>Places Index Page</h1>
-              <p>This is the Places index page</p>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
           </main>
       </Def>
-    )
+  )
   }
+  
   
 
 module.exports = index
