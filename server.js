dfd506e38705dcfe216842b579828fd2baf6d89d
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-10-19 13:40:00 -0500

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('*', (req, res) => {
-    res.render('error404')
-})
 
 // Listen for Connections
 app.listen(process.env.PORT)
